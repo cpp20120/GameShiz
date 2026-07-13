1d924b6aadabbaebf7dbe411b6e03963e03e0f96
cppshizoid <58726094+cpp20120@users.noreply.github.com>
2026-07-13 22:21:15 +0700

    feat: add Discord admin authorization options

--- framework/BotFramework.Discord/DiscordOptions.cs
+++ framework/BotFramework.Discord/DiscordOptions.cs
@@ -9,6 +9,9 @@ public sealed class DiscordOptions
     public bool Enabled { get; init; }
     public string Token { get; init; } = string.Empty;
     public string CommandPrefix { get; init; } = "!";
+    public List<ulong> AdminUserIds { get; init; } = [];
+    public List<ulong> AdminRoleIds { get; init; } = [];
+    public long AdminActorId { get; init; }
     public GatewayIntents GatewayIntents { get; init; } =
         GatewayIntents.Guilds |
         GatewayIntents.GuildMessages |
